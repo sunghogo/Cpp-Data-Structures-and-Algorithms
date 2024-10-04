34468a51e31762f5a81ead2993b875a11bed5c0c
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-10-04 16:34:01 -0400

    CPPDSA: S5/6: L26 Print List

--- Section 5 - Linked Lists/linked_list.cpp
+++ Section 5 - Linked Lists/linked_list.cpp
@@ -11,7 +11,13 @@ class Node {
         Node(int value) : value {value}, next {nullptr} {}
         int get_value() const { return value; }
         Node *get_next() const { return next; }
+        void print(std::ostream &os) const { os << value; }
 };
+
+std::ostream &operator<<(std::ostream &os, const Node *obj) {
+    obj->print(os);
+    return os;
+}
 
 class LinkedList {
     private:
@@ -20,24 +26,39 @@ class LinkedList {
         int length;
     public:
         LinkedList(int value) : head {new Node {value}}, tail {head}, length {1} {}
-        Node *get_head() const {return head;}
-        Node *get_tail() const {return tail;}
-        int get_length() const {return length;}
-        void append(int value) {}
-        void prepend(int value) {}
-        void insert(int index, int value) {}
         ~LinkedList() { 
-            for (size_t i {0} ; i < length; i++) {
+            while (head) {
                 Node *node = head;
                 head = head->get_next();
                 delete node;
             }
         }
+        Node *get_head() const {return head;}
+        Node *get_tail() const {return tail;}
+        int get_length() const {return length;}
+
+        void print_head() const { cout << "head: " << head << endl; }
+        void print_tail() const { cout << "tail: " << tail << endl; }
+        void print_length() const { cout << "length: " << length << endl; }
+        void print_list() const {
+            Node *temp = head;
+            while (temp) {
+                cout << temp->get_value() << endl;
+                temp = temp->get_next();
+            }
+        }
+
+        void append(int value) {}
+        void prepend(int value) {}
+        void insert(int index, int value) {}        
 };
 
 int main() {
     unique_ptr<LinkedList> LL {new LinkedList{4}};
-    cout << "head: " << LL->get_head()->get_value() << endl;
-    cout << "tail: " << LL->get_tail()->get_value() << endl;
-    cout << "length: " << LL->get_length() << endl;
+    
+    LL->print_head();
+    LL->print_tail();
+    LL->print_length();    
+    
+    LL->print_list();
 }
---
+++ Section 6- Linked Lists Coding Exercises/constructor.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+class Node { 
+    public:
+        int value;
+        Node* next;
+
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+        }
+}; 
+
+
+class LinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+
+    public:
+        //   +======================================================+
+        //   |                 WRITE YOUR CODE HERE                 |
+        //   | Description:                                         |
+        //   | - Write the LinkedList constructor                   |
+        //   | - Constructor takes an int value                     |
+        //   | - Create a new node                                  |
+        //   | - Initialize head, tail to the new node              |
+        //   | - Length starts at 1                                 |
+        //   |                                                      |
+        //   | Tips:                                                |
+        //   | - Use 'new' to allocate memory for the new node      |
+        //   | - Make sure to set head and tail pointers            |
+        //   | - Make sure to set length                            |
+        //   | - Check output from Test.cpp in "User logs"          |
+        //   +======================================================+
+        LinkedList(int value) : head {new Node{value}}, tail {head}, length {1} {}
+
+        ~LinkedList() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+
+        void printList() {
+            Node* temp = head;
+            if (temp == nullptr) {
+                cout << "empty";
+            } else {
+                while (temp != nullptr) {
+                    cout << temp->value;
+                    temp = temp->next;
+                    if (temp != nullptr) {
+                        cout << " -> ";
+                    }
+                }
+            }
+            cout << endl;
+        }
+
+        Node* getHead() {
+            return head;
+        }
+
+        Node* getTail() {
+            return tail; 
+        }
+
+        int getLength() {
+            return length;
+        }
+
+};
+
+int main() {
+    LinkedList *LL = new LinkedList {1};
+    cout << "head: " << LL->getHead()->value << endl;
+    cout << "tail: " << LL->getTail()->value << endl;
+    cout << "length: " << LL->getLength() << endl;
+}
