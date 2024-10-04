66e4f0d4fdabbad6ee63e3a7e1d99488ce56f905
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-10-04 17:00:32 -0400

    CPPDSA: S5/6: L29 Append

--- Section 5 - Linked Lists/linked_list.cpp
+++ Section 5 - Linked Lists/linked_list.cpp
@@ -3,28 +3,40 @@
 
 using namespace std;
 
-class Node {
+class I_Printable {
+    friend std::ostream &operator<<(std::ostream &os, const I_Printable &obj);
+public:
+    virtual void print(std::ostream &os) const = 0;
+    virtual ~I_Printable() = default;
+};
+
+std::ostream &operator<<(std::ostream &os, const I_Printable *obj) {
+    obj->print(os);
+    return os;
+}
+
+class Node : public I_Printable {
     private:
         int value;
         Node* next;
     public:
         Node(int value) : value {value}, next {nullptr} {}
+
         int get_value() const { return value; }
         Node *get_next() const { return next; }
-        void print(std::ostream &os) const { os << value; }
+        void set_value(int value) { this->value = value; }
+        void set_next(Node *next) {  this->next = next; }
+
+        virtual void print(std::ostream &os) const override { os << value; }
 };
 
-std::ostream &operator<<(std::ostream &os, const Node *obj) {
-    obj->print(os);
-    return os;
-}
-
-class LinkedList {
+class LinkedList: public I_Printable {
     private:
         Node *head;
         Node *tail;
         int length;
     public:
+        LinkedList() : head {nullptr}, tail {nullptr}, length {0} {};
         LinkedList(int value) : head {new Node {value}}, tail {head}, length {1} {}
         ~LinkedList() { 
             while (head) {
@@ -48,17 +60,30 @@ class LinkedList {
             }
         }
 
-        void append(int value) {}
+        virtual void print(std::ostream &os) const override { 
+            os << "head: " << head << endl; 
+            os  << "tail: " << tail << endl;
+            os << "length: " << length << endl;
+         }
+    
+        void append(int value) {
+            Node *node = new Node {value};
+            if (length <= 0) head = node, tail = node;
+            else {
+                tail->set_next(node);
+                length += 1;
+            }
+        }
         void prepend(int value) {}
         void insert(int index, int value) {}        
 };
 
 int main() {
     unique_ptr<LinkedList> LL {new LinkedList{4}};
+
+    cout << LL << endl;
     
-    LL->print_head();
-    LL->print_tail();
-    LL->print_length();    
-    
+    LL->append(5);
+
     LL->print_list();
 }
