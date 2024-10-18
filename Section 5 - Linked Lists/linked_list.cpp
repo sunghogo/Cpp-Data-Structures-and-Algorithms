a3068e909e2ff0eb120acde599a3f445bf14f184
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-10-18 12:37:51 -0400

    CPPDSA: S5/6: L32 Prepend

--- Section 5 - Linked Lists/linked_list.cpp
+++ Section 5 - Linked Lists/linked_list.cpp
@@ -10,8 +10,8 @@ public:
     virtual ~I_Printable() = default;
 };
 
-std::ostream &operator<<(std::ostream &os, const I_Printable *obj) {
-    obj->print(os);
+std::ostream &operator<<(std::ostream &os, const I_Printable &obj) {
+    obj.print(os);
     return os;
 }
 
@@ -21,6 +21,7 @@ class Node : public I_Printable {
         Node* next;
     public:
         Node(int value) : value {value}, next {nullptr} {}
+        Node() : Node {0} {};
 
         int get_value() const { return value; }
         Node *get_next() const { return next; }
@@ -49,21 +50,15 @@ class LinkedList: public I_Printable {
         Node *get_tail() const {return tail;}
         int get_length() const {return length;}
 
-        void print_head() const { cout << "head: " << head << endl; }
-        void print_tail() const { cout << "tail: " << tail << endl; }
-        void print_length() const { cout << "length: " << length << endl; }
-        void print_list() const {
-            Node *temp = head;
-            while (temp) {
-                cout << temp->get_value() << endl;
-                temp = temp->get_next();
-            }
-        }
-
         virtual void print(std::ostream &os) const override { 
-            os << "head: " << head << endl; 
-            os  << "tail: " << tail << endl;
-            os << "length: " << length << endl;
+            os << "[Length: " << length << "| Head: " << (head ? (*head) : Node{})  << "| Tail: " << (head ? (*head) : Node{}) << "| List: ";
+            Node *next = head;
+            while (next) {
+                os << (*next);
+                next = next->get_next();
+                if (next) os << " -> ";
+            }
+            os << "]";
          }
     
         void append(int value) {
@@ -75,16 +70,56 @@ class LinkedList: public I_Printable {
             }
             length ++;
         }
-        void prepend(int value) {}
+
+        void deleteLast() {
+            if (length == 0) return;
+            if (length == 1) {
+                head = tail = nullptr; 
+                length--;
+            }
+            else {
+                Node *pre = head;
+                Node *temp = head;
+                while (temp->get_next()) {
+                    pre = temp;
+                    temp = temp->get_next();
+                }
+                tail = pre;
+                tail->set_next(nullptr);
+                length--;
+                delete temp;
+            }
+        }
+
+        void prepend(int value) {
+            Node *node = new Node {value};
+            if (length == 0) head = tail = node;
+            else {
+                node->set_next(head);
+                head = node;
+            }
+            length++;
+        }
         void insert(int index, int value) {}        
 };
 
 int main() {
-    unique_ptr<LinkedList> LL {new LinkedList{4}};
+    unique_ptr<LinkedList> LL {new LinkedList{}};
 
-    cout << LL << endl;
+    std::cout << *LL << std::endl;
     
-    LL->append(5);
+    LL->prepend(1);
+    LL->append(2);
+    LL->append(3);
 
-    LL->print_list();
+    std::cout << *LL << std::endl;
+
+    LL->deleteLast();
+    std::cout << *LL << std::endl;
+
+    LL->deleteLast();
+    std::cout << *LL << std::endl;
+
+    LL->deleteLast();
+    std::cout << *LL << std::endl;
 }
